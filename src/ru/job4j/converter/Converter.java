054f2e4d6a29e7c366c45273a6fd62b16375898d
Viktor <mannar@inbox.ru>
2021-07-31 00:06:04 +0500

    5. Конвертер валюты

---
+++ src/ru/job4j/converter/Converter.java
@@ -0,0 +1,24 @@
+package ru.job4j.converter;
+
+public class Converter {
+
+    public static float rubleToEuro(float value) {
+        float rsl = value / 70;
+        return rsl;
+    }
+
+    public static float rubleToDollar(float value) {
+        float rsl =value / 60; 
+        return rsl;
+    }
+
+    public static void main(String[] args) {
+        float euro = Converter.rubleToEuro(140);
+        float usd =Converter.rubleToDollar(140); 
+//ниже решил сделать округление до 2 знака после точки ибо не видержала душа длинных дробей - перфекционизм.
+String resultEuro= String.format("%.2f",euro);
+String resultUsd = String.format("%.2f",usd);        
+System.out.println("140 rubles are " + resultEuro + " euro.");
+System.out.println("140 rubles are " + resultUsd + " usd.");
+}
+}
