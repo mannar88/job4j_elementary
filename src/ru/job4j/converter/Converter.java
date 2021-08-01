43f76b375385938ccb033f1601788e52e376473a
Viktor <mannar@inbox.ru>
2021-08-01 18:24:27 +0500

    0. Что такое тестирование [#159431]

--- src/ru/job4j/converter/Converter.java
+++ src/ru/job4j/converter/Converter.java
@@ -1,24 +1,32 @@
-package ru.job4j.converter;
+    package ru.job4j.converter;
 
-public class Converter {
+    public class Converter {
 
-    public static float rubleToEuro(float value) {
-        float rsl = value / 70;
-        return rsl;
+        public static float rubleToEuro(float value) {
+            return value / 70;
+        }
+
+        public static float rubleToDollar(float value) {
+            return value/60;
+        }
+
+        public static void main(String[] args) {
+            float euro = Converter.rubleToEuro(140);
+            float usd = Converter.rubleToDollar(180);
+            System.out.println("140 rubles are " + euro + " euro.");
+            System.out.println("180 rubles are " + usd + " usd.");
+
+            float in = 140;
+            float expected = 2;
+            float out = Converter.rubleToEuro(in);
+            boolean passed = expected == out;
+            System.out.println("140 rubles are 2. Test result : " + passed);
+    // проверка бакса
+            in = 180;
+            expected = 3;
+            out = Converter.rubleToDollar(in);
+            passed = expected == out;
+            System.out.println("180 rubles are 3. Test result : " + passed);
+
+        }
     }
-
-    public static float rubleToDollar(float value) {
-        float rsl =value / 60;
-        return rsl;
-    }
-
-    public static void main(String[] args) {
-        float euro = Converter.rubleToEuro(140);
-        float usd =Converter.rubleToDollar(140);
-//ниже решил сделать округление до 2 знака после точки ибо не видержала душа длинных дробей - перфекционизм.
-        String resultEuro= String.format("%.2f",euro);
-        String resultUsd = String.format("%.2f",usd);
-        System.out.println("140 rubles are " + resultEuro + " euro.");
-        System.out.println("140 rubles are " + resultUsd + " usd.");
-    }
-}
