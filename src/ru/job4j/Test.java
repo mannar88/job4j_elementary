ab8c139f9efbe5a2ccb990695fe6c10c54afaf92
Viktor <mannar@inbox.ru>
2021-08-10 21:39:21 +0500

    6.2. Упорядочить массив. [#4441]

---
+++ src/ru/job4j/Test.java
@@ -0,0 +1,11 @@
+package ru.job4j;
+
+import ru.job4j.condition.Game;
+
+import javax.swing.plaf.metal.MetalBorders;
+
+    public class Test {
+
+            public static void main(String[] args) {
+               }
+    }
