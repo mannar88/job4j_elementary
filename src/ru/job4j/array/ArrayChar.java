673497a5ce1200294b1a15d602d862d06f67bd73
Viktor <mannar@inbox.ru>
2021-08-11 19:08:09 +0500

    6.4. Слово начинается с ... [#41585 #188413/1]

--- src/ru/job4j/array/ArrayChar.java
+++ src/ru/job4j/array/ArrayChar.java
@@ -5,7 +5,7 @@ public class ArrayChar {
         boolean result = true;
         for (int i = 0; i < pref.length; i++) {
             if ( word [i] != pref [i]){
-                return  result = false;
+                return false;
             }
         }
         return result;
